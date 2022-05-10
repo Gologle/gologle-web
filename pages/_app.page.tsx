b18578fef0ecf53ce59f8fbd08bf3987bedc6ced
samueldsr <samueldsr8@gmail.com>
2022-05-10 18:14:29 -0400

    feat: Add query client & toaster to _app

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,15 +1,35 @@
 import * as React from 'react'
 import { ThemeProvider } from 'next-themes'
 import { AppProps } from 'next/app'
+import { Toaster } from 'react-hot-toast'
 import 'tailwindcss/tailwind.css'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 import '../styles/globals.css'
 
+const client = new QueryClient()
+
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <QueryClientProvider client={client}>
+      <ThemeProvider>
+        <Toaster
+          position='top-left'
+          containerStyle={{
+            top: 40,
+            left: 40,
+            bottom: 40,
+            right: 40,
+          }}
+          toastOptions={{
+            error: {
+              className: '!bg-red-50',
+            },
+          }}
+        />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
 
